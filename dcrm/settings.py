168ddfde7c784a11fbe40c12f069f7413785d45b
kensatasera <kenstasera@gmail.com>
2023-04-15 11:03:47 +0200

    registration

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = "dcrm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['Templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- website/views.py
+++ website/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 
 # Create your views here.
+def home(request):
+    #check to see if logging in 
+    if request.method == 'POST':
+        username = request.POST['first_name']
+        password = request.POST['password']
+
+        #AUTHENTICATE
+        user = authenticate(request, username = username,password =password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "you have been Logged In ")
+            return redirect ('home')
+        else:
+            messages.success(request, 'There was an error in loggin process, please try again')
+            return redirect ('home')
+    else :
+        return  render(request, 'home.html',{})
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"you have been logged out ...")
+    return redirect('home')
+
+
+def register_user(request):
+
+    return render(request, 'register.html',{})
